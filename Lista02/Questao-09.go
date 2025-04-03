f05b2431a675f1905fd134e861508c228f2a2af2
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-02 21:36:22 -0300

    Create Questao-09.go

---
+++ Lista02/Questao-09.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var compra float64
+	var venda float64
+
+	fmt.Scan(compra)
+
+	if compra < 50 {
+		venda = compra * 1.3
+	} else if compra >= 30 {
+		venda = compra * 1.4
+	} else if compra >= 10 {
+		venda = compra * 1.5
+	} else {
+		venda = compra * 1.7
+	}
+
+	fmt.Println(venda)
+
+}
