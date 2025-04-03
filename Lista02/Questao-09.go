4711a19d78af87ebf20a6a4f7a82a62344eaef3e
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-02 21:49:22 -0300

    Update Questao-09.go

--- Lista02/Questao-09.go
+++ Lista02/Questao-09.go
@@ -9,16 +9,20 @@ func main() {
 
 	fmt.Scan(compra)
 
-	if compra < 50 {
-		venda = compra * 1.3
-	} else if compra >= 30 {
-		venda = compra * 1.4
-	} else if compra >= 10 {
-		venda = compra * 1.5
-	} else {
-		venda = compra * 1.7
-	}
-
-	fmt.Println(venda)
+    if compra < 0 {
+    	if compra < 50 {
+    		venda = compra * 1.3
+    	} else if compra >= 30 {
+    		venda = compra * 1.4
+    	} else if compra >= 10 {
+    		venda = compra * 1.5
+    	} else if compra > 0 {
+    		venda = compra * 1.7
+    	}
+    	fmt.Println(venda)
+    } else {
+        fmt.Println("Valor inválido")
+    }
+    
 
 }
