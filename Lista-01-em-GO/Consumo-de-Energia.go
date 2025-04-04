e0d774f2387c9b9d5663e1f8c83ee4b098e4aca1
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 15:33:25 -0300

    Create Consumo-de-Energia.go

---
+++ Lista-01-em-GO/Consumo-de-Energia.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var salario float64
+	var energia float64
+	var custokw float64
+	var custototal float64
+	var desconto float64
+
+	fmt.Scan(salario)
+	fmt.Scan(energia)
+
+	custokw = math.Round((salario * 0.7) / 100)
+	custototal = math.Round(custokw * energia)
+	desconto = math.Round(custototal * 0.9)
+
+	fmt.Println("Custo por KW: R$", custokw)
+	fmt.Println("Custo do consumo: R$", custototal)
+	fmt.Println("Custo com desconto: R$", desconto)
+
+}
