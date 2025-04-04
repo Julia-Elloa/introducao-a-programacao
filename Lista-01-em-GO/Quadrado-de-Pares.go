80849975cb1ef14dee28077a065e1b36e6a64aaa
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 16:14:44 -0300

    Create Quadrado-de-Pares.go

---
+++ Lista-01-em-GO/Quadrado-de-Pares.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+    var i int
+
+    i = 2
+
+    fmt.Scan(n)
+
+    if 5 < n && n < 2000 {
+        for i <= n {
+			fmt.Println(i, "^", i, " = ", i * i)
+			i = i + 2
+     	 }
+    } else {
+      fmt.Println("O número deve ser maior que 5 e menor que 2000")
+    }
+
+}
