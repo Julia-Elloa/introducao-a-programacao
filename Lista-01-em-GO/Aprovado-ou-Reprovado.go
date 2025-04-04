4313a19bbbeabaf2cfd9900f34bbd17781758fc0
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 14:42:00 -0300

    Create Aprovado-ou-Reprovado.go

---
+++ Lista-01-em-GO/Aprovado-ou-Reprovado.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n1, n2, n3, media float32
+
+    fmt.Scan(n1)
+	fmt.Scan(n2)
+	fmt.Scan(n3)
+
+    media = (n1 + n2 +  n3)/3
+
+    if media >= 6 {
+      fmt.Println("MEDIA = ", media)
+      fmt.Println("APROVADO")
+    } else {
+      fmt.Println("MEDIA = ", media)
+      fmt.Println("REPROVADO")
+    }
+
+}
