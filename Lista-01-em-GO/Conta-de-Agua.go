4e9d56e00881072e47011428c7c5d64dff76b6ca
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 15:37:52 -0300

    Create Conta-de-Agua.go

---
+++ Lista-01-em-GO/Conta-de-Agua.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var conta int
+	var agua float32
+	var consumidor string
+	var valor float32
+
+	fmt.Scan(conta)
+	fmt.Scan(agua)
+	fmt.Scan(consumidor)
+
+	if consumidor == "R" || consumidor == "r" {
+		valor = (agua * 0.05) + 5
+	} else if consumidor == "C" || consumidor == "c" {
+		valor = 500 + (agua-80)*0.25
+	} else if consumidor == "I" || consumidor == "i" {
+		valor = 800 + (agua-100)*0.04
+	}
+
+	fmt.Println("\n", "CONTA = ", conta)
+	fmt.Println("VALOR DA CONTA = ", valor)
+
+}
