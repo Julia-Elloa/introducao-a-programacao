052f6fe6a442250dd0bb983f37cacea0c0166564
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 16:16:29 -0300

    Create Reajuste-Salarial.go

---
+++ Lista-01-em-GO/Reajuste-Salarial.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var salario float64
+
+    fmt.Scan(salario)
+
+    if salario <= 300 {
+      salario = salario * 1.5
+    } else {
+      salario = salario * 1.3
+    }
+
+    fmt.Println("SALARIO COM REAJUSTE = ", salario)
+
+}
