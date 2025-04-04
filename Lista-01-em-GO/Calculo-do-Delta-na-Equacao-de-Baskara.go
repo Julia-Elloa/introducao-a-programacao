ec44875bdbcf12de203bd201c6a9fbd11555be3b
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 15:56:19 -0300

    Create Calculo-do-Delta-na-Equacao-de-Baskara.go

---
+++ Lista-01-em-GO/Calculo-do-Delta-na-Equacao-de-Baskara.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var a float64
+    var b float64
+    var c float64
+    var delta float64
+
+    fmt.Scan(a)
+    fmt.Scan(b)
+    fmt.Scan(c)
+
+     delta = math.Round(b * b - (4 * a * c))
+     fmt.Println("O VALOR DE DELTA E = ", delta)
+
+}
