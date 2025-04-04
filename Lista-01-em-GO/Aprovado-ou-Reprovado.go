88d973051e381d174e21a874d834d36a6887fb17
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 20:29:44 -0300

    Update Aprovado-ou-Reprovado.go

--- Lista-01-em-GO/Aprovado-ou-Reprovado.go
+++ Lista-01-em-GO/Aprovado-ou-Reprovado.go
@@ -6,9 +6,9 @@ func main() {
 
 	var n1, n2, n3, media float32
 
-    fmt.Scan(n1)
-	fmt.Scan(n2)
-	fmt.Scan(n3)
+    	fmt.Scan(&n1)
+	fmt.Scan(&n2)
+	fmt.Scan(&n3)
 
     media = (n1 + n2 +  n3)/3
 
