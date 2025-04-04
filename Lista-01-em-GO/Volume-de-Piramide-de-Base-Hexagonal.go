690b10c08e87f09d72e4d6bfc17ce24798b8a6df
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 20:35:40 -0300

    Update Volume-de-Piramide-de-Base-Hexagonal.go

--- Lista-01-em-GO/Volume-de-Piramide-de-Base-Hexagonal.go
+++ Lista-01-em-GO/Volume-de-Piramide-de-Base-Hexagonal.go
@@ -9,8 +9,8 @@ func main() {
 
 	var altura, aresta, volume float64
     
-    fmt.Scan(altura)
-    fmt.Scan(aresta)
+    fmt.Scan(&altura)
+    fmt.Scan(&aresta)
 
     volume = math.Round((3 * aresta * aresta * math.Sqrt(3))/2)
     volume = math.Round((volume * altura)/3)
