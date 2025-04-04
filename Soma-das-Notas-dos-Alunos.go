014733e24c81928086de1bd4673e44820adfe456
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 19:50:21 -0300

    Create Soma-das-Notas-dos-Alunos.go

---
+++ Soma-das-Notas-dos-Alunos.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var alunos int
+	var nota [100]float64
+	var soma float64
+	i := 0
+
+	fmt.Println("Informe a quantidade de alunos: ")
+	fmt.Scan(alunos)
+	fmt.Println("Insira as notas: ")
+
+	for i = 0; i <= alunos; i++ {
+		fmt.Scan(nota[i])
+		soma = soma + nota[i]
+	}
+
+	fmt.Println("A soma das notas é: ", soma)
+
+}
