e5183df46f83d8705d3baadd85f59c4051cfa0ba
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-04 16:03:28 -0300

    Create Locadora-de-Charretes.go

---
+++ Lista-01-em-GO/Locadora-de-Charretes.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var horas int
+    var valor int
+
+    fmt.Scan(horas)
+
+    if horas%3 == 0 {
+      valor = horas * 10
+    } else {
+      valor = ((horas - horas%3)/3) * 10 + (horas%3) * 5
+    }
+
+    fmt.Println("O VALOR A PAGAR E = ", valor)
+
+}
