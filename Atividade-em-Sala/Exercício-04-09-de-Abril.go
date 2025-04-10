ed25acc5ae8f55ee9e5a2d4087c2176257d69af3
Julia-Elloa <juliaelloafideles@gmail.com>
2025-04-09 21:26:42 -0300

    Create Exercício-04-09-de-Abril.go

---
+++ Atividade-em-Sala/Exercício-04-09-de-Abril.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+
+	fmt.Scan(&n)
+
+	var l [10]int
+	var x int
+
+	i := 0
+	j := 0
+
+	fmt.Println("Preencha o vetor de", n, " casas:")
+	for i < n {
+		fmt.Scan(&l[i])
+		i++
+	}
+
+	fmt.Println("Escolha um valor para consultar:")
+	fmt.Scan(&x)
+
+	posicao := -1
+
+	for j < n {
+		if l[j] == x {
+			posicao = j + 1
+		}
+		j++
+	}
+
+	fmt.Println("Posição do número", x, "no vetor: ")
+	fmt.Println(posicao)
+
+}
